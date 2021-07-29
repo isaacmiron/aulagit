432322f801c224b5e4b65971e289f0ce3837055b
Isaac Miron <isaac_miron@hotmail.com>
2021-07-29 10:07:08 -0300

    inicio do projeto

---
+++ testegit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace testegit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bom dia!");
+            Console.ReadLine();
+            Console.WriteLine("Nice!");
+
+        }
+    }
+}
